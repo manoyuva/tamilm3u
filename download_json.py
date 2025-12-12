bf6b3ffdb81d4c729fe0c66740c86324e282b88e
manoyuva <236719154+manoyuva@users.noreply.github.com>
2025-12-12 17:32:44 +0530

    Create download_json.py

---
+++ download_json.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+JSON_URL = "https://sscloud7.in/multi/tamilott.json"
+
+response = requests.get(JSON_URL)
+data = response.json()
+
+with open("channels.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, indent=4, ensure_ascii=False)
+
+print("✔ JSON downloaded: channels.json")
