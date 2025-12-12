21c9c1f16cd89a3f65ccfb4e00a440a2b98a25cc
manoyuva <236719154+manoyuva@users.noreply.github.com>
2025-12-12 13:06:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import json
 
 def json_to_m3u("playlist.json", "playlist.m3u"):
-    with open(json_file, 'r', encoding='utf-8') as f:
+    with open("playlist.json", 'r', encoding='utf-8') as f:
         channels = json.load(f)
 
-    with open(m3u_file, 'w', encoding='utf-8') as f:
+    with open("playlist.m3u", 'w', encoding='utf-8') as f:
         f.write("#EXTM3U\n")
 
         for ch in channels:
@@ -17,7 +17,8 @@ def json_to_m3u("playlist.json", "playlist.m3u"):
             f.write(f'#EXTINF:-1 tvg-id="{tvg_id}" tvg-logo="{tvg_logo}" group-title="{group}",{name}\n')
             f.write(f"{url}\n")
 
-    print(f"Converted {json_file} → {m3u_file} successfully!")
+    print(f"Converted {"playlist.json"} → {"playlist.m3u"} successfully!")
 
 # Example usage
-json_to_m3u("playlist.json", "playlist.m3u")
+#json_to_m3u("playlist.json", "playlist.m3u")
+
