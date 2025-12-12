985704103335b9965b43d8549999ba7111b7e105
manoyuva <236719154+manoyuva@users.noreply.github.com>
2025-12-12 18:44:00 +0530

    Update json_to_m3u.py

--- json_to_m3u.py
+++ json_to_m3u.py
@@ -4,15 +4,15 @@ def json_to_m3u(json_file="channels.json", m3u_file="tamilott.m3u"):
     with open(json_file, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    channels = data.get("channels", [])
+    channels = data.get("channeldata", [])
 
     with open(m3u_file, "w", encoding="utf-8") as m3u:
         m3u.write("#EXTM3U\n")
 
         for ch in channels:
-            name = ch.get("name", "Unknown")
+            name = ch.get("channelname", "Unknown")
             logo = ch.get("logo", "")
-            url = ch.get("url", "")
+            url = ch.get("playbackurl", "")
 
             if not url:
                 continue  # skip channels without stream URL
