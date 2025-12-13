16d4fe0382df3d7e6ad227ffb0732146f63f00b5
manoyuva <236719154+manoyuva@users.noreply.github.com>
2025-12-13 08:40:30 +0530

    Update jsonm3u.py

--- jsonm3u.py
+++ jsonm3u.py
@@ -5,11 +5,11 @@ with open("channels.json", "r", encoding="utf-8") as f:
 
 with open("output.m3u", "w", encoding="utf-8") as f:
     f.write("#EXTM3U\n")
-    for ch in channels:
-        name = ch.get("channeldata","name", "")
-        url = ch.get("channeldata","url", "")
-        logo = ch.get("channeldata","logo", "")
-        group = ch.get("channeldata","group", "")
+    for ch in channels("channeldata"):
+        name = ch.get("name", "")
+        url = ch.get("url", "")
+        logo = ch.get("logo", "")
+        group = ch.get("group", "")
 
         f.write(
             f'#EXTINF:-1 tvg-logo="{logo}" group-title="{group}",{name}\n'
