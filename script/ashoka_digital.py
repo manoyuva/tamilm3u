f3ad5464a7dc52900bdd419d66456f712a891681
manoyuva <236719154+manoyuva@users.noreply.github.com>
2025-12-13 14:41:54 +0530

    Ashoka Digital

---
+++ script/ashoka_digital.py
@@ -0,0 +1,37 @@
+import json
+
+"""
+Custom JSON to M3U converter for OTT-style JSON
+(Supports `channeldata` array inside each content object)
+"""
+
+INPUT_JSON = "json/ashoka_digital.json"
+OUTPUT_M3U = "playlist/ashoka_digital.m3u"
+
+with open(INPUT_JSON, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+with open(OUTPUT_M3U, "w", encoding="utf-8") as m3u:
+    m3u.write("#EXTM3U\n")
+
+    for content in data:
+        group = content.get("content_title", "OTT")
+        country = content.get("content_sub_title", "")
+
+        channels = content.get("posts", [])
+        for ch in channels:
+            name = ch.get("channel_name", "Unknown")
+            url = ch.get("channel_url", "")
+            logo = ch.get("channel_image", "")
+            ch_country = ch.get("category_name", country)
+
+            m3u.write(
+                f'#EXTINF:-1 tvg-name="{name}" '
+                f'tvg-logo="{logo}" '
+                f'tvg-country="{ch_country}" '
+                f'group-title="{group}",{name}\n'
+            )
+            m3u.write(f"{url}\n")
+
+print("JSON successfully converted to M3U")
+
